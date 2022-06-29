caa281cf491edb4ce0b4820668e54b30a5a24000
Rare Raptor <connorwtuncc@gmail.com>
2022-06-29 19:27:34 -0400

    get category by id works, need to come back to create a category to fix error

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,13 +16,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
   // be sure to include its associated Products
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
